7f98dde5cd5d4d14011653a6e502252eb6686bca
spiffytech <spiffytech@gmail.com>
2015-12-06 22:49:57 -0500

    Reads a feed, prints a feed. Spends /way/ too many lines working around deficiencies in feedparser-clj wrt returning a hashmap with valid RSS keys.

--- src/rss_smash/core.clj
+++ src/rss_smash/core.clj
@@ -2,8 +2,10 @@
   (:gen-class)
   (:require [clojure.pprint :refer [pprint]]
             [http.async.client :as http]
+            [clojure.set :refer [rename-keys]]
             [byte-streams :as bs]
-            [feedparser-clj.core :as fp]))
+            [feedparser-clj.core :as fp]
+            [clj-rss.core :as rss]))
 
 (def feeds ["http://spiffy.tech/tv_torrents.rss"])
 
@@ -14,13 +16,66 @@
      (http/await)
      (http/string))))
 
+(defn munge-tags
+  "feedparser doesn't honor zero-or-one tag restrictions. Fix tags so clj-rss will accept them."
+  [item]
+  (pprint item)
+  (println)
+  (let [blacklisted-keys [:contributors :contents]
+        key-instructions [{:bad :categories :good :category :fn (partial into [])}
+                          {:bad :authors :good :author :fn first}
+                          {:bad :enclosures :good :enclosure :fn #(vector (dissoc (into {} (first %)) :uri))}
+                          {:bad :published-date :good :pubDate :fn identity}
+                          {:bad :uri :good :guid :fn #(vector {:isPermaLink false} %)}]
+        key-replacements (into {}
+                               (map
+                                #(vector (:bad %) (:good %))
+                                key-instructions))
+        singularize-values (fn [item rule]
+                             (let [k (:good rule)
+                                   v (k item)
+                                   f (:fn rule)]
+                               (if v
+                                 (assoc item k (f v))
+                                 item
+                                 )
+                               ))
+        purge-nils (fn purge-nils' [acc [k v]]
+                     (if (nil? v)
+                       acc
+                       ;(if (coll? v)
+                       ;  (assoc acc k (reduce purge-nils' {} v))
+                         (assoc acc k v)))]
+
+    (as-> item $
+      (apply dissoc $ blacklisted-keys)
+      (rename-keys $ key-replacements)
+      (reduce singularize-values $ key-instructions)
+      (reduce purge-nils {} $))))
+
+(defn mk-feed [items]
+  (println)
+  (pprint (first items))
+  (println)
+  (rss/channel-xml {  ; False means no content validation. clj-rss doesn't recognize all valid tags.
+                :title "spiffytech's TV shows"
+                :description "spiffytech's TV shows"
+                :link "http://spiffy.tech/tv_torrents.rss"}
+               items)
+
+)
+
 (defn -main
   [& args]
   (->>
    feeds
    (map fetch-feed-body)
    (map bs/to-input-stream)
-   (map fp/parse-feed)
-   (map :entries)
-   (pprint)
+   (map fp/parse-feed)  ; Is buggy with URLs, so we have to fetch them ourselves
+   (mapcat :entries)
+   (map (partial into {}))  ; feedparser returns structs
+   (map munge-tags)
+   (sort-by :published-date)
+   (mk-feed)
+   (spit "feed.xml")
   ))
