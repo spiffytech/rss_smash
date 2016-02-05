fb82433818c1347cab2c21a7ce849b1ee90011f0
spiffytech <spiffytech@gmail.com>
2016-02-04 21:37:59 -0500

    Removed old (broken) Clojure version of project

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to rss_smash
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- test/rss_smash/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns rss-smash.core-test
-  (:require [clojure.test :refer :all]
-            [rss-smash.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
